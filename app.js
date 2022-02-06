0de6dee670a50511d94c11ac2be725d65bfaa06e
swoosh <swoosh@Mac-mini.local>
2022-02-06 22:13:51 +0900

    amazon S3 setting

--- app.js
+++ app.js
@@ -1,113 +1,97 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const nunjucks = require("nunjucks");
+const dotenv = require("dotenv");
+const passport = require("passport");
+const helmet = require("helmet");
+const hpp = require("hpp");
+const redis = require("redis");
+const RedisStore = require("connect-redis")(session);
+
+dotenv.config();
+const redisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASSWORD,
+});
+const pageRouter = require("./routes/page");
+const authRouter = require("./routes/auth");
+const postRouter = require("./routes/post");
+const userRouter = require("./routes/user");
+const { sequelize } = require("./models");
+const passportConfig = require("./passport");
+const logger = require("./logger");
 
 const app = express();
-
-const port = 3001;
-
-app.set("port", port);
-
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+passportConfig(); // 패스포트 설정
+app.set("port", process.env.PORT || 8001);
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
 });
 
-app.listen(port, () => console.log("Listening on", port));
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("데이터베이스 연결 성공");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+if (process.env.NODE_ENV === "production") {
+  app.enable("trust proxy");
+  app.use(morgan("combined"));
+  app.use(helmet({ contentSecurityPolicy: false }));
+  app.use(hpp());
+} else {
+  app.use(morgan("dev"));
+}
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/img", express.static(path.join(__dirname, "uploads")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+const sessionOption = {
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+  store: new RedisStore({ client: redisClient }),
+};
+if (process.env.NODE_ENV === "production") {
+  sessionOption.proxy = true;
+  // sessionOption.cookie.secure = true;
+}
+app.use(session(sessionOption));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/", pageRouter);
+app.use("/auth", authRouter);
+app.use("/post", postRouter);
+app.use("/user", userRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  logger.info("hello");
+  logger.error(error.message);
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 module.exports = app;
-
-// const dotenv = require("dotenv");
-// const express = require("express");
-// const path = require("path");
-// const morgan = require("morgan");
-// const cookieParser = require("cookie-parser"); // 쿠키, 세션 이해하기 강좌 창고
-// const multer = require("multer");
-// const session = require("express-session");
-// const fs = require("fs");
-// const nunjucks = require("nunjucks");
-// const AWS = require("aws-sdk");
-// const multerS3 = require("multer-s3");
-
-// dotenv.config();
-// const indexRouter = require("./routes");
-// const userRouter = require("./routes/user");
-
-// const app = express();
-// app.set("port", process.env.PORT || 3000);
-// app.set("view engine", "html");
-
-// nunjucks.configure("views", {
-//   express: app,
-//   watch: true,
-// });
-
-// // 미들웨어간의 순서도 중요하다
-// app.use(morgan("dev"));
-// app.use("/", express.static(path.join(__dirname, "public")));
-// // coookie parsing
-// app.use(cookieParser(process.env.COOKIE_SECRET));
-// app.use(session());
-// // body parsing
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// try {
-//   fs.readdirSync("uploads");
-// } catch (error) {
-//   console.error("uploads 폴더가 없어 uploads 폴더를생성합니다.");
-//   fs.mkdirSync("uploads");
-// }
-
-// AWS.config.update({
-//   accessKeyId: process.env.S3_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-//   region: "ap-northeast-2",
-// });
-
-// app.use("/", indexRouter);
-
-// const upload = multer({
-//   storage: multerS3({
-//     s3: new AWS.S3(),
-//     bucket: "weddingslidetest",
-//     key(req, file, cb) {
-//       cb(null, "original/${Date.now()}${path.basename(file.originalname)}");
-//     },
-//     // destination(req, file, done) {
-//     //   done(null, "uploads/");
-//     // },
-//     // fllename(req, file, done) {
-//     //   constext = path.extname(file.originalname); // 확장자 추출 3강 참고
-//     //   done(null, path.basename(flle.originalname, ext) + Date.now() + ext);
-//     // },
-//   }),
-//   limits: { file5ize: 5 * 1024 * 1024 }, // 5MB
-// });
-
-// app.post("/img", isLoggedIn, upload.single("img"), (req, res) => {
-//   console.log(req.file);
-//   res.join({ url: req.file.location });
-// });
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./index.html"));
-// });
-
-// app.post("/upload", upload.array("many"), (req, res) => {
-//   console.log(req.files, req.body);
-//   res.send("ok");
-// });
-
-// app.get("/category/:name", (req, res) => {
-//   res.send("hi swoosh wildcare");
-// });
-
-// app.use((req, res, next) => {
-//   res.status(404).send("404 error");
-// });
-
-// app.get("*", (req, res) => {
-//   res.send("hi swoosh to all requests");
-// });
-
-// app.listen(app.get("port"), () => {
-//   console.log(app.get("port"), "waiting");
-// });
